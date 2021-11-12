ef4ce9734b1127ba86c977dd139276650d2ceb42
戴林峰 <dailinfeng66@163.com>
2021-11-12 14:54:21 +0800

    fix

--- dataops_cloud/gateway/src/main/java/com/cuit/GatewayApplication.java
+++ dataops_cloud/gateway/src/main/java/com/cuit/GatewayApplication.java
@@ -3,14 +3,13 @@ package com.cuit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author dailinfeng
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.cuit.common"})
+//@ComponentScan(basePackages = {"com.cuit.common"})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
