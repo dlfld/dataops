41cca921fdbd7ee5a62db276d519db56d1ec0768
戴林峰 <dailinfeng66@163.com>
2022-01-11 22:43:02 +0800

    fix

---
+++ dataops_cloud/common/src/main/java/com/cuit/common/model/base/file_manage/bo/OperationBo.java
@@ -0,0 +1,20 @@
+package com.cuit.common.model.base.file_manage.bo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * @Author dailinfeng
+ * @Description TODO
+ * @Date 2022/1/11 10:36 PM
+ * @Version 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class OperationBo {
+
+}
--- dataops_cloud/file_manage/src/main/java/com/cuit/file_manage/operation/intf/OperationParser.java
+++ dataops_cloud/file_manage/src/main/java/com/cuit/file_manage/operation/intf/OperationParser.java
@@ -1,13 +1,15 @@
 package com.cuit.file_manage.operation.intf;
 
+import com.cuit.common.model.base.file_manage.Operation;
 import org.springframework.stereotype.Service;
 
 /**
- * @Author dailinfeng 操作解析器
- * @Description TODO
+ * @Author dailin feng
+ * @Description 操作解析器，将操作对应到
  * @Date 2022/1/9 2:46 PM
  * @Version 1.0
  */
 @Service
 public class OperationParser {
+
 }
